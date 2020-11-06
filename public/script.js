1a26ec490b678b11f6a889a1938678601fdf044c
Janet Yu <janettyu99@gmail.com>
2020-11-05 19:26:57 -0800

    fix: change socket connection url on client

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // Make connection
-const socket = io.connect("http://localhost:4000");
+const socket = io.connect(location.origin);
 
 // DOM
 const equalBtn = document.getElementById("equal-sign");
